e6c776993fd494762b5bdfc8b6adc3fbd9e43a37
Lewis Trowbridge <lewbob9927@gmail.com>
2020-08-08 19:16:54 +0100

    Added switching functionality, with the default and WW2 options being the only ones available at the moment

--- js/switcher.js
+++ js/switcher.js
@@ -1,3 +1,19 @@
 $(".dropdown-item").click(function (){
-    console.log($(this).text())
+    let replacementSet = getReplacementSet($(this).text());
+    setReplacementSet(replacementSet);
 })
+
+function getReplacementSet(setName){
+    switch (setName) {
+        case "Default":
+            return ["Ghaul", "the Traveler", "you and your clan are", "clan", "the Vanguard", "Guardians"];
+        case "WW2":
+            return ["Hitler", "France", "you and your platoon are", "war", "platoon", "the Allies", "soldiers"];
+    }
+}
+
+function setReplacementSet(replacementSet) {
+    for (let i=1; i<6; i++){
+        $("#replace"+i.toString()).text(replacementSet[i-1]);
+    }
+}
