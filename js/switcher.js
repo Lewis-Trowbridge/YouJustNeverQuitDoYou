fd9b0a7223f5dcb887d701e3dffcd8a74c878bd4
Lewis Trowbridge <lewbob9927@gmail.com>
2020-08-08 19:33:05 +0100

    Fixed problems with array alignment and loop limit

--- js/switcher.js
+++ js/switcher.js
@@ -8,12 +8,12 @@ function getReplacementSet(setName){
         case "Default":
             return ["Ghaul", "the Traveler", "you and your clan are", "clan", "the Vanguard", "Guardians"];
         case "WW2":
-            return ["Hitler", "France", "you and your platoon are", "war", "platoon", "the Allies", "soldiers"];
+            return ["Hitler", "France", "you and your platoon are", "war", "the Allies", "soldiers"];
     }
 }
 
 function setReplacementSet(replacementSet) {
-    for (let i=1; i<6; i++){
+    for (let i=1; i<7; i++){
         $("#replace"+i.toString()).text(replacementSet[i-1]);
     }
 }
